faf877b8327df548cc5c380c3e7ea1f0bdb631d4
murat-ch <muratik.ch@gmail.com>
2020-12-04 10:44:55 -0500

    3-Done

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
 
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, files) => {
+// Set default current working directory if no argument passed in command-line
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, files) => {
     if (err) {
         console.log(err);
     }
     const statPromises = files.map((file) => {
-        return lstat(file);
+        return lstat(path.join(targetDir, file));
     });
     const allStats = await Promise.all(statPromises);
 
     for (let stats of allStats) {
         const index = allStats.indexOf(stats);
-        console.log(files[index], stats.isFile());
+        if (stats.isFile()) {
+            console.log(chalk.cyan.italic(files[index]));
+        } else {
+            console.log(chalk.magentaBright.bold(files[index]));
+        }
     }
 });
