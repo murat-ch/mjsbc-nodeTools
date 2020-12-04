37cee961faa090851ca287242b1a90a748ea6d88
murat-ch <muratik.ch@gmail.com>
2020-12-04 09:20:09 -0500

    2- Solutions with callback and promises in series

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
-const fs = require('fs');
+#!/usr/bin/env node
 
-fs.readdir(process.cwd(), (err, files) => {
-    if (true) {
+
+const fs = require('fs');
+const util = require('util');
+
+
+// Option 2.2
+// const lstat = util.promisify(fs.lstat);
+
+// Option 2.3
+// const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, files) => {
+    if (err) {
         console.log(err);
     }
-    console.log(files);
+    for (const file of files) {
+        try {
+            const stats = await lstat(file);
+            console.log(file, stats.isFile());
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
 });
+
+// Option 2.1 Manually wrap into Promise.
+const lstat  = (filename) => {
+    return new Promise((resolve, reject)=> {
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(stats);
+        })
+    });
+}
+
+/*
+
+        Option 1. Maintain an array of the results fom each lstat. As each callback is invoked, add the stats object to this
+        array. When array is full, log everything in it.
+
+const allStats = Array(files.length).fill(null);
+for (let file of files) {
+    const index = files.indexOf(file);
+
+    fs.lstat(file, (err, stats) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            allStats[index] = stats;
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(files[index], stats.isFile());
+                })
+            }
+        }
+    })
+}
+
+ */
